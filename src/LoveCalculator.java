e55f3863d1c8397597a8799f2f8c8bd7c453e032
David Gikubu <davidndungu8203@gmail.com>
2023-08-25 19:24:28 +0300

    here I created a function countOccurences of a target string within another string.

--- src/LoveCalculator.java
+++ src/LoveCalculator.java
@@ -12,5 +12,25 @@ public class LoveCalculator {
         System.out.print("Enter the second person's name: ");
         String person2 = scanner.nextLine().toUpperCase();
 
+        // Count the occurrences of "TRUE" and "LOVE" in both names
+        int trueCount = countOccurrences(person1, "TRUE") + countOccurrences(person2, "TRUE");
+        int loveCount = countOccurrences(person1, "LOVE") + countOccurrences(person2, "LOVE");
+
+        // Calculate the love score using the specified formula
+        int loveScore = trueCount * 10 + loveCount;
+
+        // Display the calculated love score
+        System.out.println("Love Score: " + loveScore);
+    }
+
+    // Function to count the occurrences of a target string within another string
+    private static int countOccurrences(String text, String target) {
+        int count = 0;
+        int index = text.indexOf(target);
+        while (index != -1) {
+            count++;
+            index = text.indexOf(target, index + 1);
+        }
+        return count;
     }
 }
